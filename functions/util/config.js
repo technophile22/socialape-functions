386f0ca33a6223e3289203a0a566823f13b7264f
technophile22 <raghavmangal22@gmail.com>
2020-07-25 18:07:36 +0530

    Organised the code in different files

---
+++ functions/util/config.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+    apiKey: "AIzaSyD_rMYig9xULSvkFZSuXNPv1xu2vW1ovzQ",
+    authDomain: "socialape-a639f.firebaseapp.com",
+    databaseURL: "https://socialape-a639f.firebaseio.com",
+    projectId: "socialape-a639f",
+    storageBucket: "socialape-a639f.appspot.com",
+    messagingSenderId: "267717782685",
+    appId: "1:267717782685:web:72ead4b3f186b925317874",
+    measurementId: "G-RBDJ9KGSQB"
+  };
---
+++ functions/util/fbAuth.js
@@ -0,0 +1,36 @@
+const { admin }= require('./admin')
+
+//Implementing a middleware for our firebase authentication
+
+
+module.exports = (req, res, next) => {
+    let idToken;
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+       idToken = req.headers.authorization.split('Bearer ')[1];
+    }
+    else{
+       console.error('No token found')
+       return res.status(403).json({error : 'Unauthorized'});
+    }
+ 
+    admin.auth().verifyIdToken(idToken)
+       .then(decodedToken => {
+          req.user = decodedToken;
+          console.log(decodedToken);
+          return db.collection('users')
+             .where('userId', '==', req.user.uid)
+             .limit(1)
+             .get();
+       })
+       .then(data => {
+          req.user.handle = data.docs[0].data().handle;
+          return next();
+       })
+       .catch(err => {
+          console.error('Error while verifying token', err);
+          return res.status(403).json(err);
+       })
+       //we are getting the user handle from the database 
+       //and attaching it to the user.request 
+ 
+ }
