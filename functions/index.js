f369c62e2d631f0a5c1968660301a594fb0fe309
technophile22 <raghavmangal22@gmail.com>
2020-07-28 12:18:39 +0530

    Implemented like, unlike and delete a scream

--- functions/index.js
+++ functions/index.js
@@ -2,18 +2,18 @@ const functions = require('firebase-functions');
 const app = require('express')();
 
 const FBAuth = require('./util/fbAuth');
-const {getAllScreams, postOneScream, getScream , commentOnScream} = require('./handlers/screams');
+const {getAllScreams, postOneScream, getScream , commentOnScream, likeScream, unlikeScream, deleteScream} = require('./handlers/screams');
 const {signup, login, uploadImage, addUserDetails, getAuthenticatedUser} = require('./handlers/users');
 
 //scream routes
 app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
 app.get('/scream/:screamId', getScream);
-//TODO 
-//delete scream
-//like a scream
-//unlike a scream
-//comment on scream
+app.delete('/scream/:screamId', FBAuth, deleteScream);
+app.get('/scream/:screamId/like', FBAuth, likeScream);
+app.get('/scream/:screamId/unlike', FBAuth, unlikeScream);
+
+
 app.post('/scream/:screamId/comment', FBAuth, commentOnScream);
 
 
